0d9cee554c06f82b5233e37c0367ed51f18f04bf
lenkton <antonov_s@yahoo.com>
2021-02-25 20:23:51 +0300

    in the question index and show feature tests the lists are being tested (as they should) instead of the single questions and tests respectively

--- spec/features/questions/index_spec.rb
+++ spec/features/questions/index_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
+QUESTIONS_LIST_SIZE = 5
+
 feature 'User can inspect the list of all questions', %q(
   In order to find a solution to my problem
   As a user
   I'd like to be able to inspect the list of questions asked before
 ) do
-  scenario 'User sees the list of headers of all previously asked questions' do
-    question = create(:question)
+  let!(:questions) { create_list(:question, QUESTIONS_LIST_SIZE) }
 
+  scenario 'User sees the list of headers of all previously asked questions' do
     visit(questions_path)
 
-    expect(page).to have_content(question.title)
+    questions.each { |question| expect(page).to have_content(question.title) }
   end
 end
