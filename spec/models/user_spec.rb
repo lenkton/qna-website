7071ef1b0a0be4df22e3a4c314c96446134ba711
lenkton <antonov_s@yahoo.com>
2021-02-26 16:23:12 +0300

    removed unnessessary tests from the User model spec and added some style fixes to it

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,29 +7,14 @@ RSpec.describe User, type: :model do
   describe '#author_of?' do
     let(:author) { create(:user) }
     let(:random_user) { create(:user) }
+    let(:question) { create(:question, author: author) }
 
-    context 'the argument is a question' do
-      let(:question) { create(:question, author: author) }
-
-      it 'returns true if the user is an author of the argument' do
-        expect(author.author_of?(question)).to eq(true)
-      end
-
-      it 'returns false if the user is not an author of the argument' do
-        expect(random_user.author_of?(question)).to eq(false)
-      end
+    it 'returns true if the user is an author of the argument' do
+      expect(author).to be_author_of(question)
     end
 
-    context 'the argument is an answer' do
-      let(:answer) { create(:answer, author: author) }
-
-      it 'returns true if the user is an author of the argument' do
-        expect(author.author_of?(answer)).to eq(true)
-      end
-
-      it 'returns false if the user is not an author of the argument' do
-        expect(random_user.author_of?(answer)).to eq(false)
-      end
+    it 'returns false if the user is not an author of the argument' do
+      expect(random_user).not_to be_author_of(question)
     end
   end
 end
