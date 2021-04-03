b309ca247f8e00030de056c2d58a2d9d4aee315d
lenkton <antonov_s@yahoo.com>
2021-04-03 13:45:19 +0300

    added simple rest api (now capable only of returning the user profile)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users
   root to: 'questions#index'
 
@@ -18,6 +19,14 @@ Rails.application.routes.draw do
   resources :files, only: [:destroy]
   resources :links, only: [:destroy]
   resources :rewards, only: [:index]
+
+  namespace :api do
+    namespace :v1 do
+      resources :profiles, only: [] do
+        get :me, on: :collection
+      end
+    end
+  end
 
   mount ActionCable.server => '/cable'
 end
